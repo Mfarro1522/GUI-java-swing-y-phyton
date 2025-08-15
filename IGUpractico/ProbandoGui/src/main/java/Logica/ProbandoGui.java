99aa41808b4665316245873f9883a3ab581ba8c0
Mauricio Fabian Farro Izquierdo <mauriciofarroizquierdo@gmail.com>
2025-08-14 22:37:55 -0500

    Creacion workspaces

---
+++ IGUpractico/ProbandoGui/src/main/java/Logica/ProbandoGui.java
@@ -0,0 +1,14 @@
+package Logica;
+import igu.Pantalla;
+/** @author mauricio*/
+public class ProbandoGui {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setLocationRelativeTo(null);//notoma ningun elemento para ubicarse
+        panta.setVisible(true);
+        
+        
+    }
+}
