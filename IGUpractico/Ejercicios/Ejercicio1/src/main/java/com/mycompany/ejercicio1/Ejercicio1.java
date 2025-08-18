4cf214be66d449f4eb6e8f19d580d41fe4689ec4
Mauricio Fabian Farro Izquierdo <mauriciofarroizquierdo@gmail.com>
2025-08-17 19:28:24 -0500

    reparar ejercicio1

---
+++ IGUpractico/Ejercicios/Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+import igu.Ventana;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
