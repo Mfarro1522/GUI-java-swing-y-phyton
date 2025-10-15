fac88476ec8c204afe5432efc93826fdea4bbe45
Mauricio Fabian Farro Izquierdo <mauriciofarroizquierdo@gmail.com>
2025-10-14 21:00:58 -0500

    Actualizacion nueva carpeta
    carpeta de ejericicos de  clase

---
+++ EjerciciosClaseGui/App01/src/CapaPresentacion/App01.java
@@ -0,0 +1,17 @@
+package CapaPresentacion;
+
+
+/*@author mauricio*/
+
+
+public class App01 {
+
+    public static void main(String[] args) {
+
+        
+        NewJDialog ventana =  new NewJDialog(null,true);
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setVisible(true);
+    }
+    
+}
