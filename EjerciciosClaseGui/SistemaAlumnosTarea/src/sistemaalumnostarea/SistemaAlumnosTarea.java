1ef0db88b9168ac81db6ecfe6da9ccccde106c4f
Mauricio Fabian Farro Izquierdo <mauriciofarroizquierdo@gmail.com>
2025-10-30 21:47:08 -0500

    implementacion doble clik

--- EjerciciosClaseGui/SistemaAlumnosTarea/src/sistemaalumnostarea/SistemaAlumnosTarea.java
+++ EjerciciosClaseGui/SistemaAlumnosTarea/src/sistemaalumnostarea/SistemaAlumnosTarea.java
@@ -1,12 +1,13 @@
 package sistemaalumnostarea;
 
 import CapaVisualzacion.jdManteniEstudiantes;
+import javax.swing.JDialog;
 
 /**** @author mauricio*/
 public class SistemaAlumnosTarea {
 
     public static void main(String[] args) {
-        
+       
         jdManteniEstudiantes dialog = new jdManteniEstudiantes(new javax.swing.JFrame(), true);
         dialog.setLocationRelativeTo(dialog);
         dialog.setVisible(true);
